379535a116e8bd73c1889f97ae44d668db6773f5
ColorfulCodes <codescolorful@gmail.com>
2019-03-04 15:36:24 -0500

    Refactor: Imported easyMode from another file.`

--- game/guess.js
+++ game/guess.js
@@ -1,4 +1,5 @@
 const readline = require('readline-sync');
+const easyMode = require('./modes').easy;
 
 let num;
 let level;
@@ -6,35 +7,6 @@ let cChoice;
 let count;
 
 console.log('Lets play Guess the Number.');
-
-function easy() {
-  count = 1;
-  cChoice = Math.floor((Math.random() * 10) + 1);
-  num = readline.question('I have my number. Guess a number between 1 and 10: ');
-  while (count <= 3) {
-    if (count === 1) {
-      count += 1;
-      if (cChoice === num) {
-        console.log('You got it!');
-        return;
-      }
-    } else if (count === 2) {
-      num = readline.question('2 more shots. Guess a number between 1 and 10: ');
-      count += 1;
-      if (cChoice === num) {
-        console.log('You got it!');
-        return;
-      }
-    } else if (count === 3) {
-      num = readline.question('Last chance. Guess a number between 1 and 10: ');
-      count += 1;
-      if (cChoice === num) {
-        console.log('You got it!');
-        return count;
-      }
-    }
-  }
-}
 
 function medium() {
   count = 1;
@@ -98,7 +70,7 @@ function coolGame() {
   level = readline.question('Pick a difficulty level (1,2, or 3): ');
 
   if (level === '1') {
-    easy();
+    easyMode();
     return;
   } else if (level === '2') {
     // random 1-100
