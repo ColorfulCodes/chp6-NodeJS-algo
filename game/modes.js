f70b42b220f0f3117b68f872c70abac8e9605c60
ColorfulCodes <codescolorful@gmail.com>
2019-03-04 15:41:08 -0500

    Refactor: Removed unused modes.

--- game/modes.js
+++ game/modes.js
@@ -33,62 +33,6 @@ function easy() {
   }
 }
 
-function medium() {
-  count = 1;
-  cChoice = Math.floor((Math.random() * 100) + 1);
-  num = readline.question('I have my number. Guess a number between 1 and 100: ');
-  while (count <= 3) {
-    if (count === 1) {
-      count += 1;
-      if (cChoice === num) {
-        console.log('You got it!');
-        return;
-      }
-    } else if (count === 2) {
-      num = readline.question('2 more shots. Guess a number between 1 and 100: ');
-      count += 1;
-      if (cChoice === num) {
-        console.log('You got it!');
-        return;
-      }
-    } else if (count === 3) {
-      num = readline.question('Last chance. Guess a number between 1 and 100: ');
-      count += 1;
-      if (cChoice === num) {
-        console.log('You got it!');
-        return count;
-      }
-    }
-  }
-}
 
-function hard() {
-  count = 1;
-  cChoice = Math.floor((Math.random() * 1000) + 1);
-  num = readline.question('I have my number. Guess a number between 1 and 1000: ');
-  while (count <= 3) {
-    if (count === 1) {
-      count += 1;
-      if (cChoice === num) {
-        console.log('You got it!');
-        return;
-      }
-    } else if (count === 2) {
-      num = readline.question('2 more shots. Guess a number between 1 and 1000: ');
-      count += 1;
-      if (cChoice === num) {
-        console.log('You got it!');
-        return;
-      }
-    } else if (count === 3) {
-      num = readline.question('Last chance. Guess a number between 1 and 1000: ');
-      count += 1;
-      if (cChoice === num) {
-        console.log('You got it!');
-        return count;
-      }
-    }
-  }
-}
 
-module.exports = { easy, medium, hard };
+module.exports = { easy };
